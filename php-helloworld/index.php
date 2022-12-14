c782af43dcecc1bee763e8a87fcd1108ff0fa4d4
MaruMarino <marimarino@frba.utn.edu.ar>
2022-12-14 13:25:22 -0500

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,9 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "A change is in the air...........................\n";
+
+$image = 'http://www.google.com/doodle4google/images/d4g_logo_global.jpg';
+$imageData = base64_encode(file_get_contents($image));
+echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+
 ?>
